abad2c6bf8dc27cb941b4ec4f0048568ead528f1
yuri glecios <glleciosyuri@gmail.com>
2024-12-31 22:29:28 -0300

    projeto inicial

---
+++ src/main/java/br/com/bidup/BidupApplication.java
@@ -0,0 +1,13 @@
+package br.com.bidup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bidup/BidupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bidup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
