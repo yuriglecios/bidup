fe957fe230ad2dd1bd192cda9e48a40a9778132f
yuri glecios <yuri.glecios@sefa.pa.gov.br>
2025-01-03 10:28:17 -0300

    ajuste controller

--- src/main/java/br/com/bidup/dadosabertos/ConsultaDadosAbertosController.java
+++ src/main/java/br/com/bidup/dadosabertos/ConsultaDadosAbertosController.java
@@ -1,6 +1,7 @@
 package br.com.bidup.dadosabertos;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ConsultaDadosAbertosController {
         this.consultaDadosAbertosService = consultaDadosAbertosService;
     }
 
+    @GetMapping("/contratos")
     public List<Contratos> consulta(
             @RequestParam(defaultValue = "1") int pagina,
             @RequestParam(defaultValue = "500") int tamanhoPagina,
